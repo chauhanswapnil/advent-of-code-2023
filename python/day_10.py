4732c7876628c338f13517d72c908b7e91ea688c
Swapnil Chauhan <swapnilchauhan999@gmail.com>
2023-12-14 16:10:31 +0000

    Day 10 Python

--- python/day_10.py
+++ python/day_10.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def read_file_as_string(file_path: str):
     with open(file_path, 'r') as file:
         return file.read()
@@ -5,66 +8,122 @@ def read_file_as_string(file_path: str):
 
 def part_1():
     inputs = read_file_as_string("../inputs/day_10.txt")
-    lines = inputs.split("\n")
-    matrix = []
-    for i in range(0, len(lines)):
-        row = []
-        for j in range(0, len(lines[i])):
-            if lines[i][j] == "S":
-                start_pos = (i, j)
-            row.append(lines[i][j])
-        matrix.append(row)
-    # print(matrix)
-    # Brute force. Go in Every Direction
-    dfs(matrix, start_pos)
+    grid = inputs.strip().splitlines()
+    for r, row in enumerate(grid):
+        for c, col in enumerate(row):
+            if col == "S":
+                start_row = r
+                start_col = c
+                break
+        else:
+            continue
+        break
+    visited = {(start_row, start_col)}
+    q = deque([(start_row, start_col)])
+
+    while q:
+        row, col = q.popleft()
+        char = grid[row][col]
+        if row > 0 and char in "S|JL" and grid[row-1][col] in "|7F" and (row-1, col) not in visited:
+            visited.add((row-1, col))
+            q.append((row-1, col))
+
+        if row < len(grid) - 1 and char in "S|7F" and grid[row+1][col] in "|JL" and (row+1, col) not in visited:
+            visited.add((row+1, col))
+            q.append((row+1, col))
+
+        if col > 0 and char in "S-J7" and grid[row][col-1] in "-LF" and (row, col-1) not in visited:
+            visited.add((row, col-1))
+            q.append((row, col-1))
+
+        if col < len(grid[row]) - 1 and char in "S-LF" and grid[row][col+1] in "-J7" and (row, col+1) not in visited:
+            visited.add((row, col+1))
+            q.append((row, col+1))
+
+    print(len(visited) // 2)
 
 
-def dfs(matrix, start):
-    rows, cols = len(matrix), len(matrix[0])
-    visited = [[False] * cols for _ in range(rows)]
+def part_2():
+    inputs = read_file_as_string("../inputs/day_10.txt")
+    grid = inputs.strip().splitlines()
+    for r, row in enumerate(grid):
+        for c, col in enumerate(row):
+            if col == "S":
+                start_row = r
+                start_col = c
+                break
+        else:
+            continue
+        break
+    visited = {(start_row, start_col)}
+    q = deque([(start_row, start_col)])
+    maybe_s = {"|", "J", "L", "7", "F", "-"}
+    while q:
+        row, col = q.popleft()
+        char = grid[row][col]
+        if row > 0 and char in "S|JL" and grid[row-1][col] in "|7F" and (row-1, col) not in visited:
+            visited.add((row-1, col))
+            q.append((row-1, col))
+            if char == "S":
+                maybe_s &= {"|", "J", "L"}
 
-    def is_valid(x, y):
-        return 0 <= x < rows and 0 <= y < cols and not visited[x][y]
+        if row < len(grid) - 1 and char in "S|7F" and grid[row+1][col] in "|JL" and (row+1, col) not in visited:
+            visited.add((row+1, col))
+            q.append((row+1, col))
+            if char == "S":
+                maybe_s &= {"|", "7", "F"}
 
-    def dfs_helper(current, prev=None):
-        # Mark the current cell as visited
-        x, y = current
-        directions = []
-        if not prev:
-            directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        if prev:
-            print(f"Previous: {prev}")
-            px, py = prev
-        visited[x][y] = True
+        if col > 0 and char in "S-J7" and grid[row][col-1] in "-LF" and (row, col-1) not in visited:
+            visited.add((row, col-1))
+            q.append((row, col-1))
+            if char == "S":
+                maybe_s &= {"-", "J", "7"}
 
-        print(f"Matrix Value at {(x, y)}: {matrix[x][y]}")
+        if col < len(grid[row]) - 1 and char in "S-LF" and grid[row][col+1] in "-J7" and (row, col+1) not in visited:
+            visited.add((row, col+1))
+            q.append((row, col+1))
+            if char == "S":
+                maybe_s &= {"-", "L", "F"}
 
-        current_char = matrix[x][y]
-        # Define the possible directions
-        if current_char == '|':
-            directions = [(-1, 0), (1, 0)]
-        elif current_char == '-':
-            directions = [(0, -1), (0, 1)]
-        elif current_char == 'L':
-            directions = [(0, 1), (-1, 0)]
-        elif current_char == 'J':
-            directions = [(0, -1), (-1, 0)]
-        elif current_char == '7':
-            directions = [(1, 0), (-1, 0)]
-        elif current_char == 'F':
-            directions = [(1, 0), (0, 1)]
-        elif current_char == '.':
-            directions = []
+    print(len(visited) // 2)
+    (S,) = maybe_s
+    print(S)
+    grid = [row.replace("S", S) for row in grid]
+    grid = ["".join(char if (r,c) in visited else "." for c, char in enumerate(row)) for r,row in enumerate(grid)]
+    print("\n".join(grid))
+    print()
+    outside = set()
+    for r, row in enumerate(grid):
+        within = False
+        up = None
+        for c, char in enumerate(row):
+            if char == "|":
+                assert up is None
+                within = not within
+            elif char == "-":
+                assert up is not None
+            elif char in "LF":
+                assert up is None
+                up = (char == "L")
+            elif char in "7J":
+                assert up is not None
+                if char != ("J" if up else "7"):
+                    within = not within
+                up = None
+            elif char == ".":
+                pass
+            else:
+                raise RuntimeError("Should not happen!")
+            if not within:
+                outside.add((r, c))
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            print("#" if (r, c) in outside-visited else ".", end="")
+        print()
 
-        # Explore neighbors
-        for dx, dy in directions:
-            new_x, new_y = x + dx, y + dy
-            if is_valid(new_x, new_y):
-                dfs_helper(current=(new_x, new_y), prev=(x, y))
-
-    # Start DFS from the given position
-    dfs_helper(start)
+    print(len(grid) * len(grid[0]) - len(outside | visited))
 
 
 if __name__ == "__main__":
     part_1()
+    part_2()
