d7f3b202540d453e837333fdfbfde8fb69473fce
Swapnil Chauhan <swapnilchauhan999@gmail.com>
2023-12-19 19:07:57 +0000

    Day 17 Python

---
+++ python/day_17.py
@@ -0,0 +1,76 @@
+from heapq import heappush, heappop
+
+
+def read_file_as_string(file_path: str):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+
+def part1():
+    inputs = read_file_as_string("../inputs/day_17.txt")
+    grid = [list(map(int, row)) for row in inputs.splitlines()]
+    seen = set()
+    # heat loss, row, col, direction row, direction col, number of moves in a direction
+    pq = [(0, 0, 0, 0, 0, 0)]
+    while pq:
+        hl, r, c, dr, dc, n = heappop(pq)
+
+        if r == len(grid) - 1 and c == len(grid[0]) - 1:
+            print(hl)
+            break
+
+        if (r, c, dr, dc, n) in seen:
+            continue
+
+        seen.add((r, c, dr, dc, n))
+
+        if n < 3 and (dr, dc) != (0, 0):
+            nr = r + dr
+            nc = c + dc
+            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                heappush(pq, (hl + grid[nr][nc], nr, nc, dr, dc, n + 1))
+
+        for ndr, ndc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            if (ndr, ndc) != (dr, dc) and (ndr, ndc) != (-dr, -dc):
+                nr = r + ndr
+                nc = c + ndc
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                    heappush(pq, (hl + grid[nr][nc], nr, nc, ndr, ndc, 1))
+
+
+def part2():
+    inputs = read_file_as_string("../inputs/day_17.txt")
+    grid = [list(map(int, row)) for row in inputs.splitlines()]
+    seen = set()
+    # heat loss, row, col, direction row, direction col, number of moves in a direction
+    pq = [(0, 0, 0, 0, 0, 0)]
+    while pq:
+        hl, r, c, dr, dc, n = heappop(pq)
+
+        if r == len(grid) - 1 and c == len(grid[0]) - 1 and n >= 4:
+            print(hl)
+            break
+
+        if (r, c, dr, dc, n) in seen:
+            continue
+
+        seen.add((r, c, dr, dc, n))
+
+        if n < 10 and (dr, dc) != (0, 0):
+            nr = r + dr
+            nc = c + dc
+            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                heappush(pq, (hl + grid[nr][nc], nr, nc, dr, dc, n + 1))
+
+        if n >= 4 or (dr, dc) == (0, 0):
+            for ndr, ndc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                if (ndr, ndc) != (dr, dc) and (ndr, ndc) != (-dr, -dc):
+                    nr = r + ndr
+                    nc = c + ndc
+                    if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                        heappush(pq, (hl + grid[nr][nc], nr, nc, ndr, ndc, 1))
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
